f2f9d6df8d2d143f905fcdc72c2d05dafbc4619d
aljacks1 <ashtonljackson@gmail.com>
2018-03-30 16:05:35 -0800

    new repo

---
+++ Root/src/Root.java
@@ -0,0 +1,8 @@
+
+public class Root {
+	public static void main(String[] args) { 
+		double number = 225;
+		System.out.println("The square root of " + number + " is " + Math.sqrt(number));
+		
+	}
+}
