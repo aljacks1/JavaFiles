06afa3b735d696c06659c65e9efb8a8b8a9cdc60
aljacks1 <ashtonljackson@gmail.com>
2018-03-30 17:15:10 -0800

    Added the Scanner Class from java.util

--- Root/src/Root.java
+++ Root/src/Root.java
@@ -1,8 +1,12 @@
-
+import java.util.Scanner;
+// added the functionality to import 
 public class Root {
 	public static void main(String[] args) { 
-		double number = 225;
-		System.out.println("The square root of " + number + " is " + Math.sqrt(number));
-		
+		double x; //declare input variable
+		Scanner scan = new Scanner(System.in); //initialize scanner for keyboard i.o
+		System.out.println("Enter a number to get it's square root!");
+		x = scan.nextDouble(); //assign the next input to our declared variable 
+		System.out.println("The square root of " + x + " is " + Math.sqrt(x));
+		//output is the square root of the number the user inputs. 
 	}
 }
