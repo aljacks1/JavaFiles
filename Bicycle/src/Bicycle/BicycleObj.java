0ad88e32ce3f2ac07b324824e83dd1efc1b88b06
aljacks1 <ashtonljackson@gmail.com>
2018-03-31 21:47:26 -0800

    main method invoking Bicycle Class

---
+++ Bicycle/src/Bicycle/BicycleObj.java
@@ -0,0 +1,11 @@
+package Bicycle;
+
+public class BicycleObj {
+
+	public static void main(String[] args) {
+		Bicycle bike1 = new Bicycle();
+		bike1.gear = 3;
+		System.out.println(bike1.gear);
+	}
+
+}
