d812445336193d21ec1a045c1ab8204e91e8450c
aljacks1 <ashtonljackson@gmail.com>
2018-03-31 21:51:47 -0800

    Added remaining methods to main method

--- Bicycle/src/Bicycle/BicycleObj.java
+++ Bicycle/src/Bicycle/BicycleObj.java
@@ -5,6 +5,8 @@ public class BicycleObj {
 	public static void main(String[] args) {
 		Bicycle bike1 = new Bicycle();
 		bike1.gear = 3;
+		bike1.cadence = 5;
+		bike1.speed = 6;
 		System.out.println(bike1.gear);
 	}
 
