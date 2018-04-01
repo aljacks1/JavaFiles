0e071de5aac6d293210a81b058582038c28d5ddc
aljacks1 <ashtonljackson@gmail.com>
2018-04-01 08:14:03 -0800

    Working with Arrays

---
+++ Bicycle/src/Bicycle/BicycleArray.java
@@ -0,0 +1,12 @@
+package Bicycle;
+
+public class BicycleArray {
+// invoke an array
+	public static void main(String[] args) {
+	int[] gearArray;
+	// declares an array
+	gearArray = new int [10];
+	// initializes an empty array with 10 spaces
+	System.out.println(gearArray.length);
+	}
+}
